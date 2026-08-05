28135db4f9e2feaefe585f18db4fccbdce3404ac
Haroon Siddiquee <s.m.haroon707@gmail.com>
2026-08-05 23:11:59 +0500

    Variables & Block vs Function Scopes

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-console.log("There are 5 main DATA types used in JavaScript")
-console.log("1. String(Double quotes, Single quotes & Back ticks)")
-console.log("2. Boolean(True or False)")
-console.log("3. Number(3,55,9.2,-24)")
-console.log("4. Undefined(It means you haven't assigned a variable yet but will later)")
-console.log("5. Null(It means it is literally empty)")
+//                                                        Variables
+
+//var is function scoped. 
+// This means it can be redeclared and reassigned.
+
+//let are block scoped. 
+// This means it can be redeclared but can't be reassigned.
+
+//const are also block scoped.
+//// This means it can't be redeclared and reassigned.
+
+var x=55
+console.log(x)
+x=33                    //This is redeclaration which doesn't returns an error.
+console.log(x)
+var x=21                //This is reassigning which doesn't returns an error.
+console.log(x)
+
+let y=64
+console.log(y)
+y=25                    //This is redeclaration which doesn't returns an error.
+console.log(y)
+// let y=12             //This is reassigning which returns an error.
+// console.log(y)
+
+const z=8
+console.log(z)
+// z=5                  //This is redeclaration which returns an error.
+// console.log(z)
+// const z=88            //This is reassigning which returns an error.
+// console.log(z)
