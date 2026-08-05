1402e444c57631b65a5660d13857a37069f68797
Haroon Siddiquee <s.m.haroon707@gmail.com>
2026-08-05 23:46:42 +0500

    Arithmetic Operators & Type Coercion

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
-//                                                        Variables
+// ------------------------------------------------------------
+// 8. ARITHMETIC OPERATORS & TYPE COERCION
+// ------------------------------------------------------------
+// Standard operators: +  -  *  /  **(exponent)  %(modulus)  ++  --
+// Shorthand: += -= *= /= %=
+//
+// TYPE COERCION: JS automatically converts types when mixing them
+// in an operation. This is one of JS's "weird" behaviors.
+ 
+// console.log(6 + "7");  -----------  "67"  -> number converted to string, then concatenated
+// console.log("7" + 6);  -----------  "76"  -> same idea, order matters for the output string
+// console.log(6 + true);  ----------  7     -> true acts like 1
+// console.log(6 + false);  ---------  6     -> false acts like 0
+// console.log("hello" + false);  ---  "hellofalse"
+ 
+// With operators OTHER than + (like * / -), JS tries to CONVERT
+// strings to numbers instead of concatenating:
+// console.log("123" * 2);  ---------  246
+// console.log("123" / 2);  ---------  61.5
+// console.log("hello" - 2);  -------  NaN ("Not a Number" - conversion failed)
 
-//var is function scoped. 
-// This means it can be redeclared and reassigned.
-
-//let are block scoped. 
-// This means it can be redeclared but can't be reassigned.
-
-//const are also block scoped.
-//// This means it can't be redeclared and reassigned.
-
-var x=55
-console.log(x)
-x=33                    //This is redeclaration which doesn't returns an error.
-console.log(x)
-var x=21                //This is reassigning which doesn't returns an error.
-console.log(x)
-
-let y=64
-console.log(y)
-y=25                    //This is redeclaration which doesn't returns an error.
-console.log(y)
-// let y=12             //This is reassigning which returns an error.
-// console.log(y)
-
-const z=8
-console.log(z)
-// z=5                  //This is redeclaration which returns an error.
-// console.log(z)
-// const z=88            //This is reassigning which returns an error.
-// console.log(z)
+const x = 5
+const y = "9"
+console.log(x+y)
+const z = true
+console.log(x+z)
+const a = false
+console.log(x+a)
+const b = "Hello "
+console.log(b+z)
+console.log(y-x)
+console.log(y-z)
+console.log(y-b)
+console.log(x*y)
+console.log(x*z)
+console.log(x*a)
+console.log(x*b)
+console.log(z*a)
+console.log(y/x)
+console.log(y/z)
+console.log(y/a)
+console.log(y/b)
+console.log(z/a)
